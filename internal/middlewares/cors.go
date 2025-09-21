d98eee239bff437fea7f85dcfd9eeb63e9203278
radifan9 <radif.aghnadiin@gmail.com>
2025-09-21 21:54:08 +0700

    fix: change CORS origin whitelist

--- internal/middlewares/cors.go
+++ internal/middlewares/cors.go
@@ -12,7 +12,7 @@ func CORSMiddleware(ctx *gin.Context) {
 	whitelist := []string{
 		"http://127.0.0.1:5500",
 		"http://localhost:5173",
-		"http://localhost:80",
+		"http://localhost",
 	}
 
 	origin := ctx.GetHeader("Origin")
