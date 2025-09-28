c72dfcc82467384d40883755474fcf054c649c6e
radifan9 <radif.aghnadiin@gmail.com>
2025-09-28 23:19:40 +0700

    fix: change cors middleware

--- internal/middlewares/cors.go
+++ internal/middlewares/cors.go
@@ -2,27 +2,28 @@ package middlewares
 
 import (
 	"net/http"
+	"slices"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CORSMiddleware(ctx *gin.Context) {
 	// Daftar origin yang diizinkan
-	// whitelist := []string{
-	// 	"http://127.0.0.1:5500",
-	// 	"http://localhost:5173",
-	// 	"http://localhost",
-	// 	"http://localhost:80",
-	// 	"http://frontend",    // Add Docker service name
-	// 	"http://frontend:80", // Add Docker service with port
-	// 	"http://192.168.100.232",
-	// }
-	// origin := ctx.GetHeader("Origin")
-	// if slices.Contains(whitelist, origin) {
-	// 	ctx.Header("Access-Control-Allow-Origin", origin)
+	whitelist := []string{
+		"http://127.0.0.1:5500",
+		"http://localhost:5173",
+		"http://localhost",
+		"http://localhost:80",
+		"http://frontend",    // Add Docker service name
+		"http://frontend:80", // Add Docker service with port
+		"http://192.168.100.232",
+	}
+	origin := ctx.GetHeader("Origin")
+	if slices.Contains(whitelist, origin) {
+		ctx.Header("Access-Control-Allow-Origin", origin)
 
-	// }
-	ctx.Header("Access-Control-Allow-Origin", "*")
+	}
+	// ctx.Header("Access-Control-Allow-Origin", "*")
 
 	// Header CORS standar
 	ctx.Header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
